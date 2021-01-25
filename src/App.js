567406ed6d452ae515c4a2e0e55f251979952e9e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-25 16:50:38 -0600

    [finish] finish useSTate core

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import {FaStar} from 'react-icons/fa'
 
 function App({name}) {
-  const [status, setStatus] = useState('not delivered');
-  const [checked, setChecked] = useState(false);
 
   return (
-    <div className="App">
-      <h1>the package is: {status}</h1>
-      <button onClick={() => setStatus("Delivered")}>Deliver</button>
-      <input type="checkbox" value={checked} 
-      onChange={() => setChecked(value => !value)}/>
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
-  );
+  <>
+    <FaStar />
+  </>);
 }
 
 export default App;
