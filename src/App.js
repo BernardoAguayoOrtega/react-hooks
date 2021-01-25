d0aff3d4a183f0d55d24b8b5ffb30e7d280e0811
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-25 16:36:17 -0600

    [add] use chekbox with use state hook

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 function App({name}) {
   const [status, setStatus] = useState('not delivered');
+  const [checked, setChecked] = useState(false);
 
   return (
     <div className="App">
       <h1>the package is: {status}</h1>
       <button onClick={() => setStatus("Delivered")}>Deliver</button>
+      <input type="checkbox" value={checked} 
+      onChange={() => setChecked(value => !value)}/>
+      <p>{checked ? "checked" : "not checked"}</p>
     </div>
   );
 }
